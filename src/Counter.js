c64f794a785cddbec5ea86c43e1df0dffe9e9f63
Abhinand <websollabsabhinand@gmail.com>
2023-08-01 10:57:03 +0530

    commit

--- src/Counter.js
+++ src/Counter.js
@@ -5,7 +5,7 @@ import {
     incrementByAmount,
     reset
 }
-from './counterSlice';
+    from './counterSlice';
 import { useState } from 'react';
 
 import React from 'react'
@@ -13,11 +13,11 @@ import React from 'react'
 import { selectCount } from './counterSlice';
 
 const Counter = () => {
-    const count = useSelector(selectCount); 
+    const count = useSelector(selectCount);
     const dispatch = useDispatch();
     //for dispatching actions
     const [incrementAmount, setIncrementAmount] = useState(0);
-    const addValue =Number(incrementAmount)||0;
+    const addValue = Number(incrementAmount) || 0;
     const resetAll = () => {
         setIncrementAmount(0);
         dispatch(reset());
@@ -25,9 +25,9 @@ const Counter = () => {
     return (
         <section>
             <p>{count}</p>
-                <div>
-                    <button onClick={() => dispatch(increment())}>+</button>
-                    <button onClick={() => dispatch(decrement())}>-</button>
+            <div>
+                <button onClick={() => dispatch(increment())}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
                 <div>
                     <div>
                         <input type="number" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
